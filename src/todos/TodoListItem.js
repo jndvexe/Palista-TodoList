bfabd396dff7f899656554c8ba98fd3d634f3b49
JonathanDavidAntonio <antoniojonathan71@gmail.com>
2023-10-06 08:17:55 +0800

    extending styled on buttons

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -2,17 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 const TodoListItemContainer = styled.div`
-    background: #000000;
+    background: white;
     border-radius: 8px;
-    border-bottom: ${props => (new Date(props.createdAt > new Date(Date.now() - 8640000 *5))
-        ? 'none'
-        : '5px solid red' )};
     margin-top: 8px;
     padding: 16px;
     position: relative;
     box-shadow: 0 4px 8px grey;
-    color: white;
+    color: black;
     text-align: left;
+`;
+const TodoItemContainerWithWarning = styled(TodoListItemContainer)`
+    border-bottom: ${props => (new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5))
+        ? 'none'
+        : '5px solid red' 
+    };
 `;
 
 const ButtonContainer = styled.div`
@@ -21,7 +24,7 @@ const ButtonContainer = styled.div`
     bottom: 12px;
 `;
 
-const CompletedButton = styled.div`
+const Button = styled.button`
     font-size: 16px;
     padding: 8px;
     border: none;
@@ -29,23 +32,21 @@ const CompletedButton = styled.div`
     outline: none;
     cursor: pointer;
     display: inline-block;
+`;
+
+const CompletedButton = styled(Button)`
     background-color: #22ee22;
 `;
 
-const RemovedButton = styled.div`
-    font-size: 16px;
-    padding: 8px;
-    border: none;
-    border-radius: 8px;
-    outline: none;
-    cursor: pointer;
-    display: inline-block;
+const RemovedButton = styled(Button)`
     background-color: #ee2222;
     margin-left: 8px;
 `;
 
-const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed}) => (
-    <TodoListItemContainer createdAt={todo.createdAt}>
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed}) => {
+    const Container = todo.isCompleted ? TodoListItemContainer : TodoItemContainerWithWarning;
+    return (
+    <Container createdAt={todo.createdAt}>
         <h3>{todo.text}</h3>
         <p>
             Created at:&nbsp;
@@ -62,8 +63,9 @@ const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed}) => (
                 onClick={() => onRemovePressed(todo.id)}
                 >Remove</RemovedButton>
         </ButtonContainer>
-    </TodoListItemContainer>
-)
+    </Container>
+    )
+};
 
 export default TodoListItem;
 
